ec618219ab245e0b034c1da674ddec444a8805fe
Angela Vawser <amvawser@gmail.com>
2019-07-17 12:09:02 -0700

    final changes to js

--- src/numbers.js
+++ src/numbers.js
@@ -5,14 +5,16 @@ const result = document.getElementById('result');
 const numberInput = document.getElementById('number-input');
 const grandPrize = document.getElementById('hi-five'); 
 const gameLost = document.getElementById('you-lose');
+let numberGuess = 4;
 
 guessButton.addEventListener('click', function(event) { 
   event.preventDefault();
-    
+  numberGuess -= 1;  
   let guessNumber = parseInt(numberInput.value);
   
   if(guessNumber < 3) {
     result.textContent = 'Oops, that\'s too low, guess again';
+
   }
   
   if(guessNumber > 3) {
@@ -23,12 +25,9 @@ guessButton.addEventListener('click', function(event) {
     result.textContent = 'CONGRATULATIONS!';
     grandPrize.classList.remove('hidden');
   }
-})
 
-// After 4 attempts if player loses:
-  // if(numberGuess === 4) {
-  //   gameLost.classList.remove('hidden');
-  // }
-// test(, (assert) => {
-//   const result = makeVolume(volume);
-//   assert.equal(result, expected);
+  if(numberGuess === 0) {
+    result.textContent = '';
+    gameLost.classList.remove('hidden');
+  }
+});
