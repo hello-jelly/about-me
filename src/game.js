4dc2de604623abf2c62ead1bca8ec87c392577a9
Angela Vawser <amvawser@gmail.com>
2019-07-18 15:38:12 -0700

    added comments to help with understanding code

--- src/game.js
+++ src/game.js
@@ -1,4 +1,8 @@
+//imported "hand" which is the result of cpu's hand randomNumber outcome
+
 import { hand } from './cpu-hand.js';
+
+//Variables for DOM elements to be used
 
 const playButton = document.getElementById('play-button');
 const playerRock = document.getElementById('player-rock');
@@ -11,8 +15,12 @@ const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 const response = document.getElementById('response');
 
+//States for tracking
+
 let wins = 0;
 let losses = 0;
+
+//Variable plays used to determine wins, losses and ties by comparing the playerHand to cpuHand
 
 const plays = {
   bai: {
@@ -32,6 +40,8 @@ const plays = {
   }
 };
 
+//Function to compare  
+
 function handleResult(result) {
   switch(result) {
     case 'win':
@@ -50,6 +60,8 @@ function handleResult(result) {
   }
 }
 
+//Switch function determined by case - Removes invisibility on corresponding img based on Player's Hand 
+
 function showPlayerHand(hand) {
   switch(hand) {
     case 'kai':
@@ -63,6 +75,8 @@ function showPlayerHand(hand) {
       break;
   }
 }
+
+//Switch function determined by case - Removes invisibility on corresponding img based on CPU's Hand 
 
 function showCpuHand(hand) {
   switch(hand) {
@@ -78,6 +92,8 @@ function showCpuHand(hand) {
   }
 }
 
+//Function to reset visibility of potential hands for each play
+
 function resetHands() {
   cpuScissor.classList.add('invisible');
   cpuRock.classList.add('invisible');
@@ -86,6 +102,8 @@ function resetHands() {
   playerRock.classList.add('invisible');
   playerPaper.classList.add('invisible');
 }
+
+//Function to increase number of wins or losses
 
 function updateTally() {
   winCount.innerText = wins;
