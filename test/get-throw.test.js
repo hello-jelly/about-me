ab794f9d66398833f4f8477226843457297bd384
Angela Vawser <amvawser@gmail.com>
2019-07-21 02:30:09 -0700

    updates to missed corrections, travis passing

--- test/get-throw.test.js
+++ test/get-throw.test.js
@@ -8,14 +8,14 @@ test('hand should give us a random scissors, rock, or paper', function(assert) {
   assert.equal(true, ['scissors', 'rock', 'paper'].includes(hand()));
 });
 
-test('getThrowFromNumber should give scissors when give a number less than .333333', function(assert) {
-  assert.equal('scissors', getThrowFromNumber(.25));
+test('getThrowFromNumber should give scissors when number equals 0', function(assert) {
+  assert.equal('scissors', getThrowFromNumber(0));
 });
 
-test('getThrowFromNumber should give rock when give a number greater than .666666', function(assert) {
-  assert.equal('rock', getThrowFromNumber(.75));
+test('getThrowFromNumber should give rock when when number equals 1', function(assert) {
+  assert.equal('rock', getThrowFromNumber(1));
 });
 
-test('getThrowFromNumber should give paper when given a number greater than .333333 and less than .666666', function(assert) {
-  assert.equal('paper', getThrowFromNumber(.5));
+test('getThrowFromNumber should give paper when when number equals 2', function(assert) {
+  assert.equal('paper', getThrowFromNumber(2));
 });
