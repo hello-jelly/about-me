3933d15890fa8aea3ffd7c028c4e92ade5b703d0
Angela Vawser <amvawser@gmail.com>
2019-07-22 11:43:13 -0700

    added is-yes.test.js to project

--- test/is-yes.test.js
+++ test/is-yes.test.js
@@ -1,18 +1,38 @@
-function isYes(text) {
-  text = text.toLowerCase();
-  if(text === 'y') {
-    return true;
-  } else if(text === 'yes') {
-    return true;
-  } else if(text === 'yeah') {
-    return true;
-  } else if(text === 'yup') {
-    return true;
-  } else if(text === 'ya') {
-    return true;
-  } else {
-    return false;
-  }
-}
+import isYes from '../src/is-yes.js';
 
-export default isYes;
+const test = QUnit.test;
+
+test('yes should be true', function(assert) {
+  const text = 'y';  
+  const expected = true;
+  const result = isYes(text);
+  assert.equal (result, expected);
+});
+
+test('yes should be true', function(assert) {
+  const text = 'yes';  
+  const expected = true;
+  const result = isYes(text);
+  assert.equal (result, expected);
+});
+
+test('yeah should be true', function(assert) {
+  const text = 'yeah';  
+  const expected = true;
+  const result = isYes(text);
+  assert.equal (result, expected);
+});
+
+test('yup should be true', function(assert) {
+  const text = 'yup';  
+  const expected = true;
+  const result = isYes(text);
+  assert.equal (result, expected);
+});
+
+test('yup should be true', function(assert) {
+  const text = 'ya';  
+  const expected = true;
+  const result = isYes(text);
+  assert.equal (result, expected);
+});
